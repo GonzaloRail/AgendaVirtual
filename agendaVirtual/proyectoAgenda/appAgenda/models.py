edc78e56d72126945dc527775a827d84eaa0c6f7
GonzaloRail <glaymem@unsa.edu.pe>
2023-07-21 00:35:28 -0500

    se hizo las migraciones correspondientes

--- agendaVirtual/proyectoAgenda/appAgenda/models.py
+++ agendaVirtual/proyectoAgenda/appAgenda/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    numero_telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
